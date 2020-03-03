e9276549655f7b485d7ccac5b2199ef6bf9c4de9
adriaserrano97 <adria.serrano.97@gmail.com>
2020-03-03 19:33:21 +0100

    BaseProject

---
+++ SpatialAudio/j1Scene.cpp
@@ -0,0 +1,93 @@
+﻿#include "p2Log.h"
+#include "j1App.h"
+#include "j1Input.h"
+#include "j1Textures.h"
+#include "j1Audio.h"
+#include "j1Render.h"
+#include "j1Window.h"
+#include "j1Scene.h"
+
+j1Scene::j1Scene() : j1Module()
+{
+	name.create("scene");
+
+}
+
+// Destructor
+j1Scene::~j1Scene()
+{}
+
+// Called before render is available
+bool j1Scene::Awake(pugi::xml_node& config)
+{
+	
+	bool ret = true;
+
+	return ret;
+}
+
+// Called before the first frame
+bool j1Scene::Start()
+{
+	
+
+	
+	App->audio->PlayMusic("audio/music/ace_of_flopdisks.ogg", 4.0F);
+		
+
+
+	debug_tex = App->tex->Load("maps/path2.png");
+	
+		
+	
+
+	return true;
+}
+
+// Called each loop iteration
+bool j1Scene::PreUpdate()
+{
+	
+
+	bool ret = true;
+
+	
+
+	return ret;
+}
+
+// Called each loop iteration
+bool j1Scene::Update(float dt)
+{
+	
+	
+	if (App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
+		App->render->camera.y -= 10;
+
+	if (App->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
+		App->render->camera.y += 10;
+
+	if (App->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
+		App->render->camera.x -= 10;
+
+	if (App->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
+		App->render->camera.x += 10;
+
+
+
+	return true;
+}
+
+// Called each loop iteration
+bool j1Scene::PostUpdate() {
+	
+		bool ret = true;
+
+	return ret;
+}
+
+// Called before quitting
+bool j1Scene::CleanUp()
+{
+	return true;
+}
---
+++ SpatialAudio/j1Textures.cpp
@@ -0,0 +1,117 @@
+#include "p2Defs.h"
+#include "p2Log.h"
+#include "j1App.h"
+#include "j1Render.h"
+#include "j1Textures.h"
+#include "SDL_image/include/SDL_image.h"
+
+#pragma comment( lib, "SDL_image/libx86/SDL2_image.lib" )
+
+j1Textures::j1Textures() : j1Module()
+{
+	name.create("textures");
+}
+
+// Destructor
+j1Textures::~j1Textures()
+{}
+
+// Called before render is available
+bool j1Textures::Awake(pugi::xml_node& config)
+{
+	
+	bool ret = true;
+	// load support for the PNG image format
+	int flags = IMG_INIT_PNG;
+	int init = IMG_Init(flags);
+
+	if((init & flags) != flags)
+	{
+		ret = false;
+	}
+
+	return ret;
+}
+
+// Called before the first frame
+bool j1Textures::Start()
+{
+	
+	bool ret = true;
+	return ret;
+}
+
+// Called before quitting
+bool j1Textures::CleanUp()
+{
+	p2List_item<SDL_Texture*>* item;
+
+	for(item = textures.start; item != NULL; item = item->next)
+	{
+		SDL_DestroyTexture(item->data);
+	}
+
+	textures.clear();
+	IMG_Quit();
+	return true;
+}
+
+// Load new texture from file path
+SDL_Texture* const j1Textures::Load(const char* path)
+{
+	
+
+	
+	SDL_Texture* texture = NULL;
+	SDL_Surface* surface = IMG_Load(path);
+
+	if(surface == NULL)
+	{
+	}
+	else
+	{
+		texture = LoadSurface(surface);
+		SDL_FreeSurface(surface);
+	}
+
+	return texture;
+}
+
+// Unload texture
+bool j1Textures::UnLoad(SDL_Texture* texture)
+{
+	p2List_item<SDL_Texture*>* item;
+
+	for(item = textures.start; item != NULL; item = item->next)
+	{
+		if(texture == item->data)
+		{
+			SDL_DestroyTexture(item->data);
+			textures.del(item);
+			return true;
+		}
+	}
+	return false;
+}
+
+// Translate a surface into a texture
+SDL_Texture* const j1Textures::LoadSurface(SDL_Surface* surface)
+{
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(App->render->renderer, surface);
+
+	if(texture == NULL)
+	{
+	}
+	else
+	{
+		textures.add(texture);
+	}
+
+	return texture;
+}
+
+// Retrieve size of a texture
+void j1Textures::GetSize(const SDL_Texture* texture, uint& width, uint& height) const
+{
+	SDL_QueryTexture((SDL_Texture*)texture, NULL, NULL, (int*) &width, (int*) &height);
+}
